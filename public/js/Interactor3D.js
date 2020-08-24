061bf2d4108bdf3a27f23be3482962ec4d220890
Brian Santoso <brian.santoso@berkeley.edu>
2020-08-24 12:01:28 -0700

    Reworked Interactor3D

--- public/js/Interactor3D.js
+++ public/js/Interactor3D.js
@@ -2,64 +2,159 @@ import ChessGame from "./ChessGame.js";
 
 class Interactor3D {
 	constructor(team, chessGame, rayCaster) {
-		this._movePreviewer = new MovePreviewer(ChessGame.OMNISCIENT, chessGame, rayCaster);
-		this._pieceSelector = new PieceSelector(team, chessGame, rayCaster);
-		this._moveConfirmer = new MoveConfirmer(ChessGame.GHOST, chessGame, rayCaster);
+		this._team = team;
+		this._game = chessGame;
 		this._rayCaster = rayCaster;
 		
-		this._unselected = () => {
-			this._movePreviewer.update();
-			this._pieceSelector.update();
-			this._moveConfirmer.update();
-		}
-		this._selected = () => {
-			this._pieceSelector.update();
-			this._moveConfirmer.update();
-		}
+		this._movePreviewer = new MovePreviewer(this, ChessGame.OMNISCIENT);
+		this._pieceSelector = new PieceSelector(this, team);
+		this._moveConfirmer = new MoveConfirmer(this, ChessGame.GHOST);
+		
+		// Define behavior for unselected state
+		this._unselected = {
+			update: () => {
+				if (this._myTurn()) {
+					this._movePreviewer.update();
+					this._movePreviewer.showMovesFor(this._movePreviewer.hovering());
+				}
+				
+				this._pieceSelector.update();
+				// TODO: make _pieceSelector highlight hovering piece
+			},
+			onclick: () => {
+				if (this._myTurn()) {
+					this._pieceSelector.update(); // update _pieceSelector's hovering
+					this._pieceSelector.select(); // set _pieceSelector's selected to its hovering
+					if (this._pieceSelector.selected()) { // if clicked on a ghost piece
+						// Hide _movePreviewer's moves
+						this._movePreviewer.showMovesFor(null);
+						// Show the moves for what was selected
+						this._pieceSelector.showMovesFor(this._pieceSelector.selected());
+						// TODO: make _pieceSelector highlight selected piece
+						
+						// Swap to selected state
+						this.swapState(this._selected);
+					}
+				}
+			},
+			onSwapOut: () => {}
+		};
+		
+		// Define behavior for selected state
+		this._selected = {
+			update: () => {
+				this._moveConfirmer.update();
+				// Can optinally do something more, like highlight the ghost move that is being hovering on.
+			},
+			onclick: () => {
+				this._moveConfirmer.update();
+				this._moveConfirmer.select();
+				if (this._moveConfirmer.selected()) {
+					let move = this._moveConfirmer.selected().move;
+					this.offerMove(move);
+				}
+				this._moveConfirmer.setSelected(null);
+				this._pieceSelector.setSelected(null);
+				this._pieceSelector.showMovesFor(null);
+				this.swapState(this._unselected);
+			},
+			onSwapOut: () => {}
+		};
 		
 		this._state = this._unselected;
 	}
 	
 	setRayCaster(rayCaster) {
-		[this._movePreviewer, this._pieceSelector, this._moveConfirmer].forEach(component => {
-			component.setRayCaster(rayCaster);
-		});
+		this._rayCaster = rayCaster;
 	}
 	
 	intentionalClick(event) {
-		if (this._pieceSelector.canClick()) {
-			let selected = this._pieceSelector.onclick();
-			if (selected) {
-				this._state = this._selected;
-			} else {
-				this._state = this._unselected;
-			}
-		}
-		this._moveConfirmer.onclick();
+		this._state.onclick();
 	}
 	
 	update() {
-		this._state();
+		this._state.update();
+	}
+	
+	swapState(state) {
+		this._state.onSwapOut();
+		this._state = state;
+	}
+	
+	boardGraphics() {
+		return this._game.boardGraphics();
+	}
+	
+	getPossibleMoves(piece) {
+		return this._game.getPossibleMoves(piece);
+	}
+	
+	rayCast(team) {
+		team = team || this.team; // default to Interactor's team
+		return this.boardGraphics().rayCast(this._rayCaster, team);
+	}
+	
+	offerMove(move) {
+		this._game.makeMove(move);
+	}
+	
+	_myTurn() {
+		return this._team === this._game.currTurn();
 	}
 }
 
+Interactor3D.isPiece = function(mesh) {
+	return mesh && !!mesh.piece;
+}
+	
+Interactor3D.isGhost = function(mesh) {
+	return mesh && !!mesh.move;
+}
+
 class Interactor3DWorker {
-	constructor(team, chessGame, rayCaster) {
+	constructor(parent, team) {
+		this._parent = parent;
+		this._team = team;
+		
 		this._hovering = null;
 		this._selected = null;
-		this._rayCaster = rayCaster;
-		this._showingMovesFor = new Map();
-		
-		this._team = team;
-		this._game = chessGame;
+		this._showingMovesFor = null;
 	}
 	
 	update() {
-		
+		let hovering = this._rayCast();
+		this._setHovering(hovering);
 	}
 	
-	setRayCaster(rayCaster) {
-		this._rayCaster = rayCaster;
+	showMovesFor(mesh, preview=false) {
+		if (Interactor3D.isPiece(mesh)) {
+			let piece = mesh.piece;
+			if (this._showingMovesFor !== mesh) {
+				// If different than already showing, hide previous and show new
+				this._hidePossibleMoves(this._showingMovesFor);
+				this._showPossibleMoves(mesh, preview);
+			}
+		} else {
+			this._hidePossibleMoves(this._showingMovesFor);
+		}
+		this._showingMovesFor = mesh;
+	}
+	
+	hovering() {
+		return this._hovering;
+	}
+	
+	select() {
+		// Set selected to hovering (requires updating hovering through update())
+		this.setSelected(this._hovering);
+	}
+	
+	selected() {
+		return this._selected;
+	}
+	
+	_rayCast() {
+		return this._parent.rayCast(this._team);
 	}
 	
 	_setHovering(mesh) {
@@ -68,131 +163,50 @@ class Interactor3DWorker {
 		return different;
 	}
 	
-	_setSelected(mesh) {
+	setSelected(mesh) {
 		let different = this._selected !== mesh;
 		this._selected = mesh;
 		return different;
 	}
 	
-	_getHovering() {
-		return this._hovering;
-	}
-	
-	_getSelected() {
-		return this._selected;
+	_boardGraphics() {
+		return this._parent.boardGraphics();
 	}
 	
 	_getPossibleMoves(piece) {
-		return this._game.getPossibleMoves(piece);
-	}
-	
-	_boardGraphics() {
-		return this._game.boardGraphics();
+		return this._parent.getPossibleMoves(piece);
 	}
 	
 	_showPossibleMoves(mesh, preview=false) {
-		if (this._isPiece(mesh)) {
-//			console.log('showing', mesh)
+		if (Interactor3D.isPiece(mesh)) {
 			let piece = mesh.piece;
 			let moves = this._getPossibleMoves(piece);
-			this._boardGraphics().showPossibleMoves(piece, moves, preview, 12);
+			this._boardGraphics().showPossibleMoves(piece, moves, preview, 8);
 		}
 	}
 	
 	_hidePossibleMoves(mesh) {
-		if (this._isPiece(mesh)) {
-//			console.log('hiding', mesh)
+		if (Interactor3D.isPiece(mesh)) {
 			let piece = mesh.piece;
-			this._boardGraphics().hidePossibleMoves(piece, 12);
+			this._boardGraphics().hidePossibleMoves(piece, 8);
 		}
 	}
 	
-	_isPiece(mesh) {
-		return mesh && !!mesh.piece;
-	}
-	
-	_isGhost(mesh) {
-		return mesh && !!mesh.move;
-	}
-	
-	_rayCast() {
-		return this._boardGraphics().rayCast(this._rayCaster, this._team);
-	}
+
 }
 
 class MovePreviewer extends Interactor3DWorker {
-	update() {
-		let prevHovering = this._getHovering();
-		let hovering = this._rayCast();
-		let different = this._setHovering(hovering);
-		
-		if (different) {
-			this._hidePossibleMoves(prevHovering);
-			this._showPossibleMoves(hovering);
-		}
-	}
-	
-	_showPossibleMoves(mesh) {
-		super._showPossibleMoves(mesh, true);
+	showMovesFor(mesh) {
+		super.showMovesFor(mesh, true);
 	}
 }
 
 class PieceSelector extends Interactor3DWorker /* Implements Clicker */ {
-	update() {
-		let prevHovering = this._getHovering();
-		let hovering = this._rayCast();
-		this._setHovering(hovering);
-	}
-	
-	canClick() {
-		return this._team === this._game.currTurn();
-	}
-	
-	onclick() {
-		let prevSelected = this._getSelected();
-		let different = this._setSelected(this._getHovering());
-		
-		if (this._getSelected()) {
-			if (different) {
-				this._hidePossibleMoves(prevSelected);
-				this._hidePossibleMoves(this._getSelected());
-				this._showPossibleMoves(this._getSelected());
-			} else {
-				
-			}
-			
-//			this._hidePossibleMoves(this._getSelected());
-//			this._showPossibleMoves(this._getSelected());
-			return true;
-		} else {
-			this._hidePossibleMoves(prevSelected);
-			return false;
-		}
-	}
+
 }
 
 class MoveConfirmer extends Interactor3DWorker /* Implements Clicker */ {
-	update() {
-		let prevHovering = this._getHovering();
-		let hovering = this._rayCast();
-		this._setHovering(hovering);
-	}
-	
-	canClick() {
-		return this._team === this._game.currTurn();
-	}
-	
-	onclick() {
-		if (this._isGhost(this._getHovering())) {
-			this._offerMove(this._getHovering().move);
-//			this._setHovering(null);
-		}
-	}
-	
-	_offerMove(move) {
-		// TODO: change
-		this._game.makeMove(move);
-	}
+
 }
 
 export default Interactor3D;
