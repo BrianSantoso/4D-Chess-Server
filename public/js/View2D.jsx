2be1d08cd8b64b3f2bbe5385920018c042ca6128
Brian Santoso <brian.santoso@berkeley.edu>
2020-11-18 00:10:19 -0800

    Worked on UI prototype

--- public/js/View2D.jsx
+++ public/js/View2D.jsx
@@ -2,27 +2,47 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import WhiteIcon from '../assets/player/king_white.svg';
 import BlackIcon from '../assets/player/king_black.svg';
+import ChessGame from './ChessGame.js';
 
 class View2D {
 	constructor() {
+//		this.root = (
+//			<div>
+//				<PlayerInfo team={ChessGame.WHITE}></PlayerInfo>
+//				<PlayerInfo team={ChessGame.BLACK}></PlayerInfo>
+//			</div>
+//		);
 		this.root = (
 			<div>
-				<PlayerInfo></PlayerInfo>
 			</div>
 		);
 	}
 	
 	draw() {
+		// TODO: just call draw method once on load
 		ReactDOM.render(this.root, document.getElementById('gui'));
 	}
 }
 
 class PlayerInfo extends Component {
 	
+	constructor(props) {
+		super(props);
+		this.state = {
+			team: this.props.team,
+			time: 0,
+			playerName: '',
+		}
+	}
+	
 	render() {
+		const icon = this.props.team === ChessGame.BLACK ? BlackIcon : WhiteIcon;
+		
 		return (
 			<div>
-				test
+				<img src={icon}></img>
+				<div> {this.state.playerName} </div>
+				<div> {this.state.time} </div>
 			</div>
 		);
 	}
